f11886561afffb3197c2d527d287b621b424e658
IsraFC <israfc27@gmail.com>
2026-04-20 22:52:35 -0600

    Dashboard principal: Filtros avanzados y optimización de estructura

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     
     <link rel="stylesheet" href="css/estilos.css">
 </head>
@@ -30,11 +31,15 @@
         <div class="collapse navbar-collapse" id="mainNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link px-3" href="index.php"><i class="bi bi-house-door"></i> Inicio</a></li>
-                <li class="nav-item"><a class="nav-link px-3" href="maquinas.php"><i class="bi bi-cpu"></i> Inventario</a></li>
-                <li class="nav-item"><a class="nav-link px-3" href="registro_ticket.php"><i class="bi bi-plus-circle"></i> Nuevo Ticket</a></li>
+                <li class="nav-item"><a class="nav-link px-3" href="maquinas.php"><i class="bi bi-cpu"></i> Maquinas</a></li>
             </ul>
         </div>
     </div>
 </nav>
 
 <div class="container py-5">
+
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
