754244df8d507fb7414780632617e2256cf634e0
IsraFC <israfc27@gmail.com>
2026-07-13 01:03:39 -0600

    fix: habilitar debug de errores en login

--- login.php
+++ login.php
@@ -4,6 +4,11 @@
  * @package Portal_Demex
  * @brief Interfaz gráfica con validación específica de errores y mitigación precisa de fuerza bruta.
  */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
